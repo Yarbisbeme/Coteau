7ef975a192a27987469eb106e1ead97620394b95
yarbisbeme <yarbisbeme@gmail.com>
2025-03-05 18:00:31 -0400

    App Done

---
+++ screens/_index.js
@@ -0,0 +1,20 @@
+
+import PokemonScreen from "./pokemon-screen";
+import HomeScreen from "./home-screen";
+import AboutScreen from "./about-screen";
+import AgePredictorScreen from "./age-predictor-screen";
+import GenderPrediction from "./gender-prediction";
+import UniversitiesScreen from "./universities-screen";
+import WeatherScreen from "./weather-screen";
+import WordpresNewsScreen from "./wordpress-news-screen";
+
+export default {
+    PokemonScreen,
+    HomeScreen,
+    AboutScreen,
+    AgePredictorScreen,
+    GenderPrediction,
+    UniversitiesScreen,
+    WeatherScreen,
+    WordpresNewsScreen,
+}
